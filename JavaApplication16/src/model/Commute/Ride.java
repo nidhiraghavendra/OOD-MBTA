ccc613ca8053b022e2cc7f6557e7f7b08539cba2
Chandana Harish <harish.c@northeastern.edu>
2023-04-02 12:06:11 -0400

    Ride csv file with code

--- JavaApplication16/src/model/Commute/Ride.java
+++ JavaApplication16/src/model/Commute/Ride.java
@@ -5,6 +5,10 @@
  */
 package model.Commute;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import model.RideAgent.RideAgent;
 import model.Routes.Location;
@@ -23,7 +27,46 @@ public class Ride extends Commute {
         this.agent = new RideAgent();
     }
     
+    ArrayList<Route> route = new ArrayList<Route>();
+    
     public ArrayList<Route> getCommuteRoutes() {
-        return null;
+    	String file = "C:\\Users\\chand\\OOD PROJECT\\ride.csv";
+    	String line = "";
+    	
+        try {
+        	BufferedReader reader = new BufferedReader(new FileReader(file));
+        	while((line = reader.readLine()) != null) {
+        		String[] row = line.split(",");
+        		
+        		Route rideRoute = new Route();
+        		Location location = new Location();
+        		location.setName("Brookline");
+        		rideRoute.setSource(location);
+        		rideRoute.getDestination().setName("Copley");
+        		rideRoute.setDistance(2);
+        		rideRoute.setDuration(10);
+        		rideRoute.getCurrentLocation().setName("ABC");
+        		
+        		route.add(rideRoute);
+        		
+    		}
+        	
+        }catch(Exception e) {
+        	e.printStackTrace();
+          }
+    	
+        return route;
+        
+        
+        
     }
+
+	public ArrayList<Route> getRoute() {
+		return route;
+	}
+
+	public void setRoute(ArrayList<Route> route) {
+		this.route = route;
+	}
+    
 }
--- JavaApplication16/src/model/Routes/Location.java
+++ JavaApplication16/src/model/Routes/Location.java
@@ -14,7 +14,16 @@ public class Location {
     private String name;
         
     public Location() {
-        
+        locationID++;
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
     
 }
